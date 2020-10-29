157915d30138154178275fc8832b798c0714462e
Carlos Augusto Borges <calimaborges@gmail.com>
2020-10-29 03:54:38 -0300

    see if static props works for env vars

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ export default function Index({ mailTo }) {
   return <div><h1>Hello, {mailTo}!</h1></div>;
 }
 
-export function getServerSideProps() {
+export function getStaticProps() {
   return {
     props: {
       mailTo: process.env.NOTE_EMAIL || null
-    }
+    },
+    revalidate: 1
   }
 }
