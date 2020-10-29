cde9b16401ff8fd927532d146aae80ea163e9203
Carlos Augusto Borges <calimaborges@gmail.com>
2020-10-29 03:43:53 -0300

    static props test failed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,10 @@ export default function Index({ mailTo }) {
   return <h1>Hello, {mailTo}!</h1>;
 }
 
-export function getStaticProps() {
+export function getServerSideProps() {
   return {
     props: {
       mailTo: process.env.NOTE_EMAIL || null
-    },
-    revalidate: 1,
+    }
   }
 }
