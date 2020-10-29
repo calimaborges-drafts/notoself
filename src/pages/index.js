f8ad566ce83aafedb33759f43e0572a293ec1a59
Carlos Augusto Borges <calimaborges@gmail.com>
2020-10-29 03:33:27 -0300

    fix static props

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ export default function Index({ mailTo }) {
   return <h1>Hello, {mailTo}!</h1>;
 }
 
-export default function getStaticProps() {
+export function getStaticProps() {
   return {
     props: {
-      mailTo: process.env.NOTE_EMAIL
+      mailTo: process.env.NOTE_EMAIL || null
     },
     revalidate: 1,
   }
