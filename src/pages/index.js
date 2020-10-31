3a8d8bed906205ba9c55ccf353a3f37aa7111b05
Carlos Augusto Borges <calimaborges@gmail.com>
2020-10-31 19:07:24 -0300

    API to send email using mailgun done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,36 @@
+import { useRouter } from "next/router";
+
 export default function Index({ mailTo }) {
-  return <div><h1>Hello, {mailTo}!</h1></div>;
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const noteEl = event.target.elements.namedItem("note");
+    const response = await fetch("/api/note", {
+      method: "POST",
+      body: JSON.stringify({
+        note: noteEl.value,
+        key: null, // FIXME: Register should store and this should get it
+        domain: null // FIXME: Register should store and this should get it
+      }),
+    });
+    noteEl.value = "";
+    const json = await response.json();
+  }
+
+  return (
+    <div>
+      <h1>Hello, {mailTo}!</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea name="note" />
+        <button type="submit">Send note</button>
+      </form>
+    </div>
+  );
 }
 
 export function getStaticProps() {
   return {
     props: {
-      mailTo: process.env.NOTE_EMAIL || null
-    }
-  }
+      mailTo: process.env.NOTE_EMAIL || null,
+    },
+  };
 }
